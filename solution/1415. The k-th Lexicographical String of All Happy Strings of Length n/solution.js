8132d39b1e2d485e692adb32bae3b5f3fbe076e8
tzuyi <a0988426059@gmail.com>
2023-07-11 17:18:05 +0800

    feat: add solution to 1415. The k-th Lexicographical String of All Happy Strings of Length n

---
+++ solution/1415. The k-th Lexicographical String of All Happy Strings of Length n/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getHappyString = function(n, k) {
+    const LETTERS = ['a', 'b', 'c'];
+    const backtracking = (current) => {
+        if (current.length === n) {
+            k -= 1;
+            return k ? '' : current;
+        }
+        for (let index = 0; index < LETTERS.length; index++) {
+            const char = LETTERS[index];
+            if (current.at(-1) === char) continue;
+            const value = backtracking(current + char);
+
+            if (value) return value;
+        }
+        return '';
+    };
+
+    return backtracking('');
+};
