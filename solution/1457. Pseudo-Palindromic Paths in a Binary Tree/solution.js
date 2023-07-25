8a05460bb6f2dd244973fd021fb107845fffa26f
tzuyi <a0988426059@gmail.com>
2023-07-25 15:29:39 +0800

    feat: add solution to 1457. Pseudo-Palindromic Paths in a Binary Tree

---
+++ solution/1457. Pseudo-Palindromic Paths in a Binary Tree/solution.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var pseudoPalindromicPaths  = function(root) {
+    const getPalindromicPaths = (node = root, hash = new Set()) => {
+        if (!node) return 0;
+        const { val } = node;
+        const isPalindromic = hash.has(val);
+
+        isPalindromic ? hash.delete(val) : hash.add(val);
+        const left = getPalindromicPaths(node.left, hash);
+        const right =  getPalindromicPaths(node.right, hash);
+        const isPalindromicPath =  hash.size < 2;
+
+        isPalindromic ? hash.add(val) : hash.delete(val);
+        if (!node.left && !node.right) return isPalindromicPath ? 1 : 0;
+        return left + right; 
+    };
+
+    return getPalindromicPaths();
+};
