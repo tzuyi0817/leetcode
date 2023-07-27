61d26d1c9ee42054c8f6f4804f694a0b4374a047
tzuyi <a0988426059@gmail.com>
2023-07-27 14:12:49 +0800

    feat: add solution to 1466. Reorder Routes to Make All Paths Lead to the City Zero

---
+++ solution/1466. Reorder Routes to Make All Paths Lead to the City Zero/solution.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var minReorder = function(n, connections) {
+    const routesSet = new Set();
+    const connectionMap = connections.reduce((map, [from, to]) => {
+        const connectionFrom = map.get(from) ?? [];
+        const connectionTo = map.get(to) ?? [];
+
+        routesSet.add(`${from}_${to}`);
+        connectionFrom.push(to);
+        connectionTo.push(from);
+        map.set(from, connectionFrom);
+        return map.set(to, connectionTo);
+    }, new Map());
+
+    const getReorderCount = (route, parent) => {
+        const currnetConnections = connectionMap.get(route);
+
+        return currnetConnections.reduce((count, connection) => {
+            if (parent === connection) return count;
+            const isReorder = routesSet.has(`${route}_${connection}`);
+
+            return count + getReorderCount(connection, route) + (isReorder ? 1 : 0);
+        }, 0);
+    };
+
+    return getReorderCount(0, -1);
+};
