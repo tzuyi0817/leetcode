031852f36efa145c7b067e3a8bd59645f38c33cd
tzuyi <a0988426059@gmail.com>
2023-08-15 10:34:51 +0800

    feat: add solution to 1519. Number of Nodes in the Sub-Tree With the Same Label

---
+++ solution/1519. Number of Nodes in the Sub-Tree With the Same Label/solution.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {string} labels
+ * @return {number[]}
+ */
+var countSubTrees = function(n, edges, labels) {
+    const result = [];
+    const visited = new Set([0]);
+    const edgesMap = edges.reduce((map, [a, b]) => {
+        const edgeA = map.get(a) ?? [];
+        const edgeB = map.get(b) ?? [];
+
+        edgeA.push(b);
+        edgeB.push(a);
+        map.set(a, edgeA);
+        return map.set(b, edgeB);
+    }, new Map());
+
+    const countEdges = (node) => {
+        const label = labels[node];
+        const targetEdges = edgesMap.get(node) ?? [];
+        const countMap = targetEdges.reduce((map, edge) => {
+            if (visited.has(edge)) return map;
+            visited.add(edge);
+            const currentCountMap = countEdges(edge);
+
+            currentCountMap.forEach((value, key) => {
+                const count = map.get(key) ?? 0;
+                map.set(key, count + value);
+            });
+            return map;
+        }, new Map([[label, 1]]));
+
+        result[node] = countMap.get(label);
+        return countMap;
+    };
+
+    countEdges(0);
+    return result;
+};
