96f28189f41bf6c72d0dd0aa3d3f0a214f035e9e
tzuyi <a0988426059@gmail.com>
2023-08-31 10:29:59 +0800

    feat: add solution to 1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers

---
+++ solution/1577. Number of Ways Where Square of Number Is Equal to Product of Two Numbers/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var numTriplets = function(nums1, nums2) {
+    const triplets = (numsA, numsB) => {
+        const squareMap = numsA.reduce((map, num) => {
+            const square = num ** 2;
+            const count = map.get(square) ?? 0;
+
+            return map.set(square, count + 1);
+        }, new Map());
+        let result = 0;
+
+        for (let a = 0; a < numsB.length; a++) {
+            for (let b = a - 1; b >= 0; b--) {
+                const product = numsB[a] * numsB[b];
+
+                if (!squareMap.has(product)) continue;
+                result += squareMap.get(product);
+            }
+        }
+        return result;
+    };
+
+    return triplets(nums1, nums2) + triplets(nums2, nums1);
+};
