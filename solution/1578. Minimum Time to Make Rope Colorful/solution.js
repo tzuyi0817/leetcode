3dfb03d3030e774ce602bd19752ae69480364e9a
tzuyi <a0988426059@gmail.com>
2023-09-01 10:13:00 +0800

    feat: add solution to 1578. Minimum Time to Make Rope Colorful

---
+++ solution/1578. Minimum Time to Make Rope Colorful/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ */
+var minCost = function(colors, neededTime) {
+    let currentColor = '';
+    let result = currentMaxTime = 0;
+
+    for (let index = 0; index < colors.length; index++) {
+        const color = colors[index];
+        const time = neededTime[index];
+
+        if (currentColor === color) {
+            result += Math.min(time, currentMaxTime);
+            currentMaxTime = Math.max(time, currentMaxTime);
+        } else {
+            currentColor = color;
+            currentMaxTime = time;
+        }
+    }
+    return result;
+};
