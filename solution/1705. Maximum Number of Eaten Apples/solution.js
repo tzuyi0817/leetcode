c1347416c60d50d63096a4c353c98861c3894866
tzuyi <a0988426059@gmail.com>
2023-10-12 09:55:17 +0800

    feat: add solution to 1705. Maximum Number of Eaten Apples

---
+++ solution/1705. Maximum Number of Eaten Apples/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} apples
+ * @param {number[]} days
+ * @return {number}
+ */
+var eatenApples = function(apples, days) {
+    const applesTime = Array(4 * 10 ** 4 + 1).fill(0);
+    let result = 0;
+    let currentTime = Number.MAX_SAFE_INTEGER;
+    let last = apples.length
+
+    for (let index = 0; index <= last; index++) {
+        const count = apples[index];
+
+        currentTime = Math.max(index, currentTime);
+        if (count) {
+            const rottenTime = index + days[index] - 1;
+
+            applesTime[rottenTime] += count;
+            currentTime = Math.min(currentTime, rottenTime);
+            last = Math.max(last, rottenTime);
+        }
+        while (!applesTime[currentTime] && currentTime < last) currentTime += 1;
+        if (!applesTime[currentTime]) continue;
+        result += 1;
+        applesTime[currentTime] -= 1;
+    }
+    return result;
+};
