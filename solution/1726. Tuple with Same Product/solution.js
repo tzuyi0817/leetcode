156d9e2bb66f463537f1a29b68e0408de223dacc
tzuyi <a0988426059@gmail.com>
2023-10-19 09:43:16 +0800

    feat: add solution to 1726. Tuple with Same Product

---
+++ solution/1726. Tuple with Same Product/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var tupleSameProduct = function(nums) {
+    const productMap = new Map();
+    let result = 0;
+
+    for (let a = 1; a < nums.length; a++) {
+        for (let b = 0; b < a; b++) {
+            const product = nums[a] * nums[b];
+            const count = productMap.get(product) ?? 0;
+
+            result += 8 * count;
+            productMap.set(product, count + 1);
+        }
+    }
+    return result;
+};
