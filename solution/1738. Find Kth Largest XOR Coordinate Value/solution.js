d0d363bcc599b11a795729b20a66be507f67ca30
tzuyi <a0988426059@gmail.com>
2023-10-24 10:21:19 +0800

    feat: add solution to 1738. Find Kth Largest XOR Coordinate Value

---
+++ solution/1738. Find Kth Largest XOR Coordinate Value/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthLargestValue = function(matrix, k) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    const dp =  Array(m + 1).fill('').map(_ => Array(n + 1).fill(0));
+    const result = [];
+
+    for (let row = 1; row <= m; row++) {
+        for (let col = 1; col <= n; col++) {
+            dp[row][col] = 
+                dp[row - 1][col] ^ 
+                dp[row][col - 1] ^ 
+                dp[row - 1][col - 1] ^
+                matrix[row - 1][col - 1];
+
+            result.push(dp[row][col]);
+        }
+    }
+    result.sort((a, b) => b - a);
+    return result[k - 1];
+};
