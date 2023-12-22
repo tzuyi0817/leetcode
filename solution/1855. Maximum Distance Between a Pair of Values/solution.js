0307cac72f30e453903e438493d2a19729f95d3c
tzuyi <a0988426059@gmail.com>
2023-12-22 15:09:43 +0800

    feat: add solution to 1855. Maximum Distance Between a Pair of Values

---
+++ solution/1855. Maximum Distance Between a Pair of Values/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDistance = function(nums1, nums2) {
+    const MAX_SIZE = nums2.length;
+    let result = end = 0;
+
+    for (let start = 0; start < nums1.length; start++) {
+        const num1 = nums1[start];
+
+        while (end < MAX_SIZE && num1 <= nums2[end]) {
+            result = Math.max(end - start, result);
+            end += 1; 
+        }
+    }
+    return result;
+};
