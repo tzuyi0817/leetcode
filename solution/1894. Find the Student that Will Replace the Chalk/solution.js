a3ad9146ddebc8d7802befb7f6d047b0cabe1692
tzuyi <a0988426059@gmail.com>
2024-01-05 14:56:17 +0800

    feat: add solution to 1894. Find the Student that Will Replace the Chalk

---
+++ solution/1894. Find the Student that Will Replace the Chalk/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} chalk
+ * @param {number} k
+ * @return {number}
+ */
+var chalkReplacer = function(chalk, k) {
+    const roundOfChalk = chalk.reduce((total, count) => total + count);
+    let remainChalk = k % roundOfChalk;
+
+    if (remainChalk === 0) return 0;
+
+    for (let index = 0; index < chalk.length; index++) {
+        remainChalk -= chalk[index];
+
+        if (remainChalk >= 0) continue;
+        return index;
+    }
+    return 0;
+};
