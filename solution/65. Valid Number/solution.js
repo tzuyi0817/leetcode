0cdec6ad08921453b991bf7bca86756ea23255ca
tzuyi <a0988426059@gmail.com>
2024-05-08 09:43:29 +0800

    feat: add solution to 65. Valid Number

---
+++ solution/65. Valid Number/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isNumber = function(s) {
+    let seenNumber = seenDot = seenE = false;
+
+    for (let index = 0; index < s.length; index++) {
+        const char = s[index];
+
+        if (Number.isInteger(+char)) {
+            seenNumber = true;
+        } else if (char === '.') {
+            if (seenDot || seenE) return false;
+            seenDot = true;
+        } else if (/[eE]/.test(char)) {
+            if (seenE || !seenNumber) return false;
+            seenE = true;
+            seenNumber = false;
+        } else if (/[+-]/.test(char)) {
+            if (index === 0) continue;
+            if (s[index - 1].toLowerCase() !== 'e') return false;
+        } else {
+            return false;
+        }
+    }
+    return seenNumber;
+};
