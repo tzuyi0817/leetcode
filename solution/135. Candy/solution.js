87c097621cadc2b75ba291e77d162de6fa0e81ba
tzuyi <a0988426059@gmail.com>
2024-05-14 11:29:28 +0800

    feat: add solution to 135. Candy

---
+++ solution/135. Candy/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+    const n = ratings.length;
+    const candies = Array(n).fill(1);
+
+    for (let index = 0; index < n - 1; index++) {
+        if (ratings[index + 1] <= ratings[index]) continue;
+        candies[index + 1] = candies[index] + 1;
+    }
+    for (let index = n - 1; index > 0; index--) {
+        if (ratings[index - 1] <= ratings[index]) continue;
+        if (candies[index - 1] > candies[index]) continue;
+        candies[index - 1] = candies[index] + 1;
+    }
+    return candies.reduce((result, count) => result + count);
+}
