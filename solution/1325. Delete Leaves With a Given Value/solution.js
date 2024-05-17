7dad5626347ccfae58f8584ae1d60e54916656eb
tzuyi <a0988426059@gmail.com>
2024-05-17 11:52:04 +0800

    feat: add solution to 1325. Delete Leaves With a Given Value

---
+++ solution/1325. Delete Leaves With a Given Value/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {TreeNode}
+ */
+var removeLeafNodes = function(root, target) {
+    const removeNode = (node) => {
+        if (!node) return null;
+        const { val, right, left } = node;
+
+        node.left = removeNode(left);
+        node.right = removeNode(right);
+
+        if (!node.left && !node.right && val === target) return null;
+        return node;
+    };
+
+    return removeNode(root);
+};
