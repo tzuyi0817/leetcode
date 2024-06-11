9ff5bd92d7a77f7d54ec101c4ef01ec70f881aef
tzuyi <a0988426059@gmail.com>
2024-06-11 13:46:45 +0800

    feat: add solution to 458. Poor Pigs

---
+++ solution/458. Poor Pigs/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} buckets
+ * @param {number} minutesToDie
+ * @param {number} minutesToTest
+ * @return {number}
+ */
+var poorPigs = function(buckets, minutesToDie, minutesToTest) {
+    const times = Math.floor(minutesToTest / minutesToDie);
+    let pigs = 0;
+    let total = 1;
+
+    while (total < buckets) {
+        total *= (times + 1);
+        pigs += 1;
+    }
+    return pigs;
+};
