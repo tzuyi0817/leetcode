3c5cf86d409c6d9c12a41f0f5a4e8f5259f598d4
tzuyi <a0988426059@gmail.com>
2024-07-03 11:04:18 +0800

    feat: add solution to 685. Redundant Connection II

---
+++ solution/685. Redundant Connection II/solution.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var findRedundantDirectedConnection = function(edges) {
+    const graph = new Map();
+    const n = edges.length;
+    let firstEntrance = secondEntrance = null;
+
+    for (const edge of edges) {
+        const [parent, child] = edge;
+
+        if (graph.has(child)) {
+            firstEntrance = [graph.get(child), child];
+            secondEntrance = [parent, child];
+            edge[1] = 0;
+            continue;
+        }
+        graph.set(child, parent);
+    }
+    
+    const unionFind = (node) => {
+        if (!graph.has(node)) return node;
+        return unionFind(graph.get(node));
+    };
+
+    graph.clear();
+
+    for (const [parent, child] of edges) {
+        const from = unionFind(parent);
+        const to = unionFind(child);
+
+        if (from === to) return firstEntrance ?? [parent, child];
+        graph.set(to, from);
+    }
+    return secondEntrance;
+};
