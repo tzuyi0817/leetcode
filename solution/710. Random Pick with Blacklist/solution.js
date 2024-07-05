a7d9a90b4067eb29ddb3d6a3f1f5b6706620a204
tzuyi <a0988426059@gmail.com>
2024-07-05 11:23:10 +0800

    feat: add solution to 710. Random Pick with Blacklist

---
+++ solution/710. Random Pick with Blacklist/solution.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} n
+ * @param {number[]} blacklist
+ */
+var Solution = function(n, blacklist) {
+    let current = n - 1;
+
+    this.boundary = n - blacklist.length;
+    this.blacklistMap = new Map();
+
+    for (const num of blacklist) {
+        this.blacklistMap.set(num, -1);
+    }
+
+    for (const num of blacklist) {
+        if (num >= this.boundary) continue;
+    
+        while (this.blacklistMap.has(current)) current -= 1;
+        this.blacklistMap.set(num, current);
+        current -= 1;
+    }
+};
+
+/**
+ * @return {number}
+ */
+Solution.prototype.pick = function() {
+    const random = Math.floor(Math.random() * this.boundary);
+
+    return this.blacklistMap.get(random) ?? random;
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(n, blacklist)
+ * var param_1 = obj.pick()
+ */
