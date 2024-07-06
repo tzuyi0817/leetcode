0aa08132c120cb2de298603b76cff54ad96fcc40
tzuyi <a0988426059@gmail.com>
2024-07-06 14:45:19 +0800

    feat: add solution to 2582. Pass the Pillow

---
+++ solution/2582. Pass the Pillow/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @param {number} time
+ * @return {number}
+ */
+var passThePillow = function(n, time) {
+    const isReverse = Math.floor(time / (n - 1)) % 2;
+    const position = time % (n - 1);
+
+    return isReverse ? n - position : position + 1;
+};
