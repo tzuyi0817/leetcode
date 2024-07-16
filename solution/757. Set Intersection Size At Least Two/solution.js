1fed6b95422313dbf235c8546ac91119deda988d
tzuyi <a0988426059@gmail.com>
2024-07-16 10:54:10 +0800

    feat: add solution to 757. Set Intersection Size At Least Two

---
+++ solution/757. Set Intersection Size At Least Two/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var intersectionSizeTwo = function(intervals) {
+    let last1 = last2 = -1;
+    let result = 0;
+
+    intervals.sort((a, b) => a[1] - b[1] || b[0] - a[0]);
+
+    for (const [start, end] of intervals) {
+        if (start <= last1) continue;
+        if (start > last2) {
+            last1 = end - 1;
+            last2 = end;
+            result += 2;
+            continue;
+        }
+        last1 = last2;
+        last2 = end;
+        result += 1;
+    }
+    return result;
+};
