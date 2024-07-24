4615bff963d49eb5510ecfa82d1a86bffb74e6b3
tzuyi <a0988426059@gmail.com>
2024-07-24 13:21:02 +0800

    feat: add solution to 2191. Sort the Jumbled Numbers

---
+++ solution/2191. Sort the Jumbled Numbers/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} mapping
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortJumbled = function(mapping, nums) {
+    const mappingNums = nums.map((num, index) => {
+        let mappingNum = '';
+
+        for (const char of `${num}`) {
+            mappingNum += mapping[char];
+        }
+        return { mappingNum: +mappingNum, index };
+    });
+
+    mappingNums.sort((a, b) => a.mappingNum - b.mappingNum);
+
+    return mappingNums.map(({ index }) => nums[index]);
+};
