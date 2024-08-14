eb60b6e5fdb218c83bc6927536e4eb93a74f57d6
tzuyi <a0988426059@gmail.com>
2024-08-14 13:16:02 +0800

    feat: add solution to 879. Profitable Schemes

---
+++ solution/879. Profitable Schemes/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @param {number} minProfit
+ * @param {number[]} group
+ * @param {number[]} profit
+ * @return {number}
+ */
+var profitableSchemes = function(n, minProfit, group, profit) {
+    const MODULO = 10 ** 9 + 7;
+    const groups = group.length;
+    const dp = Array(groups + 1).fill('').map(_ => Array(n + 1).fill('').map(_ => Array(minProfit + 1).fill(-1)));
+
+    const commitCrimes = (index, currentProfit, members) => {
+        if (index >= groups) return currentProfit >= minProfit ? 1 : 0;
+        if (dp[index][members][currentProfit] !== -1) return dp[index][members][currentProfit];
+
+        let result = commitCrimes(index + 1, currentProfit, members);
+        
+        if (members >= group[index]) {
+            const newProfit = Math.min(minProfit, currentProfit + profit[index]);
+
+            result += commitCrimes(index + 1, newProfit, members - group[index]);
+            result %= MODULO;
+        }
+        return dp[index][members][currentProfit] = result;
+    };
+
+    return commitCrimes(0, 0, n);
+};
