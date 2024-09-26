176888554b5e20cdcabd6629753a12d413146ee7
tzuyi <a0988426059@gmail.com>
2024-09-26 15:54:49 +0800

    feat: add solution to 729. My Calendar I

---
+++ solutions/729. My Calendar I/solution.js
@@ -0,0 +1,24 @@
+var MyCalendar = function () {
+  this.calendar = [];
+};
+
+/**
+ * @param {number} start
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function (start, end) {
+  for (const event of this.calendar) {
+    if (end > event.start && start < event.end) return false;
+  }
+
+  this.calendar.push({ start, end });
+
+  return true;
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
