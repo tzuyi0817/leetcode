5db7283f38f28d547fbd881d0fb8dcc070fa292f
tzuyi <a0988426059@gmail.com>
2024-10-02 12:52:20 +0800

    feat: add solution to 1331. Rank Transform of an Array

---
+++ solutions/1331. Rank Transform of an Array/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function (arr) {
+  const n = arr.length;
+  const sortedArr = arr.map((num, index) => ({ num, index })).sort((a, b) => a.num - b.num);
+  const result = Array(n);
+  let rank = 0;
+  let previous = Number.MIN_SAFE_INTEGER;
+
+  for (const { num, index } of sortedArr) {
+    if (previous !== num) rank += 1;
+
+    result[index] = rank;
+    previous = num;
+  }
+  return result;
+};
