c36494bfa763c1510a55eb4906cda1fa4b5ae2c2
tzuyi <a0988426059@gmail.com>
2024-10-02 13:28:38 +0800

    feat: add solution to 982. Triples with Bitwise AND Equal To Zero

---
+++ solutions/982. Triples with Bitwise AND Equal To Zero/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countTriplets = function (nums) {
+  const maxNum = Math.max(...nums);
+  const counts = Array(maxNum + 1).fill(0);
+  let result = 0;
+
+  for (const a of nums) {
+    for (const b of nums) {
+      const AND = a & b;
+
+      counts[AND] += 1;
+    }
+  }
+
+  for (const a of nums) {
+    for (let b = 0; b <= maxNum; b++) {
+      if (a & b) continue;
+
+      result += counts[b];
+    }
+  }
+  return result;
+};
