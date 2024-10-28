d887ef6bbc773295aca3d57689866e5e6ce79f6f
tzuyi <a0988426059@gmail.com>
2024-10-28 09:35:25 +0800

    feat: add solution 2501. Longest Square Streak in an Array

---
+++ solutions/2501. Longest Square Streak in an Array/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestSquareStreak = function (nums) {
+  const n = nums.length;
+  const memo = new Map();
+  let result = 0;
+
+  nums.sort((a, b) => a - b);
+
+  for (const num of nums) {
+    const sqrt = Math.sqrt(num);
+    const length = memo.has(sqrt) ? memo.get(sqrt) + 1 : 1;
+
+    memo.set(num, length);
+    result = Math.max(length, result);
+  }
+  return result > 1 ? result : -1;
+};
