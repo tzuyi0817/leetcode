1e1ce158db00dfef09c8eb45230e51988c8706e7
tzuyi <a0988426059@gmail.com>
2024-12-03 12:07:05 +0800

    feat: add solution 1224. Maximum Equal Frequency

---
+++ solutions/1224. Maximum Equal Frequency/solution.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxEqualFreq = function (nums) {
+  const n = nums.length;
+  const freqMap = new Map();
+  const countMap = new Map();
+  let maxCount = 0;
+  let result = 0;
+
+  for (let index = 0; index < n; index++) {
+    const num = nums[index];
+    let count = countMap.get(num) ?? 0;
+
+    if (count && freqMap.has(count)) {
+      freqMap.set(count, freqMap.get(count) - 1);
+    }
+    count += 1;
+    countMap.set(num, count);
+    freqMap.set(count, (freqMap.get(count) ?? 0) + 1);
+    maxCount = Math.max(maxCount, count);
+
+    if (
+      maxCount === 1 ||
+      freqMap.get(maxCount) * maxCount === index ||
+      (freqMap.get(maxCount - 1) + 1) * (maxCount - 1) === index
+    ) {
+      result = index + 1;
+    }
+  }
+  return result;
+};
