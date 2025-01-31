96bcf220f15805994fcefc93e60d032df742fad5
tzuyi <a0988426059@gmail.com>
2025-01-31 09:38:01 +0800

    refactor: solution `827. Making A Large Island`

--- solutions/827. Making A Large Island/solution.js
+++ solutions/827. Making A Large Island/solution.js
@@ -5,47 +5,48 @@
 const largestIsland = function (grid) {
   const n = grid.length;
 
-  const getIslandSize = (row, col, id) => {
-    if (row < 0 || col < 0 || row >= n || col >= n) return 0;
+  const getGroupArea = (row, col, group) => {
+    if (row >= n || col >= n || row < 0 || col < 0) return 0;
     if (grid[row][col] !== 1) return 0;
 
-    grid[row][col] = id;
+    grid[row][col] = group;
 
-    const top = getIslandSize(row - 1, col, id);
-    const down = getIslandSize(row + 1, col, id);
-    const left = getIslandSize(row, col - 1, id);
-    const right = getIslandSize(row, col + 1, id);
+    const leftArea = getGroupArea(row, col - 1, group);
+    const rightArea = getGroupArea(row, col + 1, group);
+    const upperArea = getGroupArea(row - 1, col, group);
+    const lowerArea = getGroupArea(row + 1, col, group);
 
-    return 1 + top + down + left + right;
+    return 1 + leftArea + rightArea + upperArea + lowerArea;
   };
-  const islands = Array.from({ length: n ** 2 + 2 }).fill(0);
-  let groupId = 2;
+  const groupMap = new Map();
+  let group = 2;
 
   for (let row = 0; row < n; row++) {
     for (let col = 0; col < n; col++) {
-      if (!grid[row][col]) continue;
+      if (grid[row][col] !== 1) continue;
+      const area = getGroupArea(row, col, group);
 
-      islands[groupId] = getIslandSize(row, col, groupId);
-      groupId += 1;
+      groupMap.set(group, area);
+      group += 1;
     }
   }
   let result = 0;
 
   for (let row = 0; row < n; row++) {
     for (let col = 0; col < n; col++) {
-      if (grid[row][col] > 0) continue;
-      const top = grid[row - 1]?.[col] ?? 0;
-      const down = grid[row + 1]?.[col] ?? 0;
-      const left = grid[row][col - 1] ?? 0;
-      const right = grid[row][col + 1] ?? 0;
-      const islandGroups = new Set([top, down, left, right]);
-      let size = 1;
+      if (grid[row][col] !== 0) continue;
+      const leftGroup = grid[row][col - 1];
+      const rightGroup = grid[row][col + 1];
+      const upperGroup = grid[row - 1]?.[col];
+      const lowerGroup = grid[row + 1]?.[col];
+      const groups = new Set([leftGroup, rightGroup, upperGroup, lowerGroup]);
+      let area = 1;
 
-      for (const groupId of islandGroups) {
-        size += islands[groupId];
+      for (const group of groups) {
+        area += groupMap.get(group) ?? 0;
       }
-      result = Math.max(size, result);
+      result = Math.max(area, result);
     }
   }
-  return result ? result : n ** 2;
+  return result ? result : n * n;
 };
