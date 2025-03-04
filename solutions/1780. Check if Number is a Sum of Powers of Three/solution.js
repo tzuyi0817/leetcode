33702862bce334eb9d6c3bf39b5a6043915eb046
tzuyi <a0988426059@gmail.com>
2025-03-04 09:28:59 +0800

    refactor: solution `1780. Check if Number is a Sum of Powers of Three`

--- solutions/1780. Check if Number is a Sum of Powers of Three/solution.js
+++ solutions/1780. Check if Number is a Sum of Powers of Three/solution.js
@@ -2,9 +2,13 @@
  * @param {number} n
  * @return {boolean}
  */
-const checkPowersOfThree = function (n) {
-  if (n === 1) return true;
-  if (n % 3 === 0) return checkPowersOfThree(n / 3);
-  if (n % 3 === 1) return checkPowersOfThree((n - 1) / 3);
-  return false;
+var checkPowersOfThree = function (n) {
+  if (n <= 1) return true;
+  const baseLog = Math.floor(Math.log(n) / Math.log(3));
+  const power = Math.pow(3, baseLog);
+  const nextN = n - power;
+
+  if (nextN >= power) return false;
+
+  return checkPowersOfThree(nextN);
 };
