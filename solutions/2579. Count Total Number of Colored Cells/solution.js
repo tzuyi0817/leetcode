573cef662d0513c5ec3c4b09d135cce8c31b5c2d
tzuyi <a0988426059@gmail.com>
2025-03-05 09:27:17 +0800

    feat: add solution `2579. Count Total Number of Colored Cells`

---
+++ solutions/2579. Count Total Number of Colored Cells/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var coloredCells = function (n) {
+  const lifting = 4;
+  const base = 1;
+
+  // Arithmetic progression
+  // S = Number of terms / 2 × (first term + last term)
+
+  return base + ((n - 1) / 2) * (lifting * n);
+};
