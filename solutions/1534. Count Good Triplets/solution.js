b441e523089769769582f83280bb39344a07ebba
tzuyi <a0988426059@gmail.com>
2025-04-14 09:16:09 +0800

    feat: add solution `1534. Count Good Triplets`

---
+++ solutions/1534. Count Good Triplets/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+const countGoodTriplets = function (arr, a, b, c) {
+  const n = arr.length;
+  let result = 0;
+
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+      if (Math.abs(arr[i] - arr[j]) > a) continue;
+
+      for (let k = j + 1; k < n; k++) {
+        if (Math.abs(arr[j] - arr[k]) > b) continue;
+        if (Math.abs(arr[i] - arr[k]) > c) continue;
+
+        result += 1;
+      }
+    }
+  }
+
+  return result;
+};
