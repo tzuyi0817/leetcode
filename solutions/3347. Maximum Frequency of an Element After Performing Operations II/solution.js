680d3297094c15df502576c8e9a46ef16da228ed
tzuyi <a0988426059@gmail.com>
2025-10-22 09:36:39 +0800

    feat: add solution `3347. Maximum Frequency of an Element After Performing Operations II`

---
+++ solutions/3347. Maximum Frequency of an Element After Performing Operations II/solution.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} numOperations
+ * @return {number}
+ */
+const maxFrequency = function (nums, k, numOperations) {
+  const countMap = new Map();
+  const lineMap = new Map();
+  const candidates = new Set();
+  let adjustable = 0;
+  let result = 1;
+
+  for (const num of nums) {
+    const startNum = num - k;
+    const endNum = num + k + 1;
+    const count = countMap.get(num) ?? 0;
+    const startCount = lineMap.get(startNum) ?? 0;
+    const endCount = lineMap.get(endNum) ?? 0;
+
+    countMap.set(num, count + 1);
+    lineMap.set(startNum, startCount + 1);
+    lineMap.set(endNum, endCount - 1);
+    candidates.add(num);
+    candidates.add(startNum);
+    candidates.add(endNum);
+  }
+
+  const sortedCandidates = [...candidates].sort((a, b) => a - b);
+
+  for (const num of sortedCandidates) {
+    if (lineMap.has(num)) {
+      adjustable += lineMap.get(num);
+    }
+
+    const count = countMap.get(num) ?? 0;
+    const adjusted = adjustable - count;
+    const operations = Math.min(adjusted, numOperations);
+
+    result = Math.max(operations + count, result);
+  }
+
+  return result;
+};
