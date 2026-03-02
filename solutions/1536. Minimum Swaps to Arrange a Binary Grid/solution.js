491b1975eeaac06980de948bb83cc5f28febe1bb
tzuyi <a0988426059@gmail.com>
2026-03-02 09:44:22 +0800

    refactor(solutions): solution `1536. Minimum Swaps to Arrange a Binary Grid`

--- solutions/1536. Minimum Swaps to Arrange a Binary Grid/solution.js
+++ solutions/1536. Minimum Swaps to Arrange a Binary Grid/solution.js
@@ -4,22 +4,30 @@
  */
 const minSwaps = function (grid) {
   const n = grid.length;
-  const zeroSuffixCounts = grid.map(row => n - row.lastIndexOf(1) - 1);
+  const rows = grid.map(row => n - row.lastIndexOf(1) - 1);
   let result = 0;
 
-  for (let row = 0; row < n; row++) {
-    const targetZeroSuffixCounts = n - row - 1;
-    let swapRow = row;
+  const findSwapRow = (row, needZeros) => {
+    for (let index = row; index < n; index++) {
+      if (rows[index] >= needZeros) {
+        return index;
+      }
+    }
 
-    while (swapRow < n && zeroSuffixCounts[swapRow] < targetZeroSuffixCounts) swapRow += 1;
-    if (swapRow === n) return -1;
-    for (let currentRow = swapRow; currentRow > row; currentRow--) {
-      [zeroSuffixCounts[currentRow], zeroSuffixCounts[currentRow - 1]] = [
-        zeroSuffixCounts[currentRow - 1],
-        zeroSuffixCounts[currentRow],
-      ];
+    return -1;
+  };
+
+  for (let row = 0; row < n - 1; row++) {
+    const needZeros = n - row - 1;
+    const swapRow = findSwapRow(row, needZeros);
+
+    if (swapRow === -1) return -1;
+
+    for (let index = swapRow; index > row; index--) {
+      [rows[index - 1], rows[index]] = [rows[index], rows[index - 1]];
       result += 1;
     }
   }
+
   return result;
 };
