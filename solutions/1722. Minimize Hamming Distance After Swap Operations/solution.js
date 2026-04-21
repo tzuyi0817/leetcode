c1ca666a3205d6830b4c1c0de0034f0cfa468dab
tzuyi <a0988426059@gmail.com>
2026-04-21 09:42:14 +0800

    refactor(solutions): solution `1722. Minimize Hamming Distance After Swap Operations`

--- solutions/1722. Minimize Hamming Distance After Swap Operations/solution.js
+++ solutions/1722. Minimize Hamming Distance After Swap Operations/solution.js
@@ -5,33 +5,74 @@
  * @return {number}
  */
 const minimumHammingDistance = function (source, target, allowedSwaps) {
-  const size = source.length;
-  const union = new Array(size).fill('').map((_, index) => index);
-  const swapMap = new Map();
-  const find = node => (union[node] === node ? node : find(union[node]));
+  const n = source.length;
+  const uf = new UnionFind(n);
+  const sourceMap = new Map();
   let result = 0;
 
   for (const [a, b] of allowedSwaps) {
-    const parentA = find(a);
-    const parentB = find(b);
-
-    if (parentA === parentB) continue;
-    union[parentA] = parentB;
+    uf.union(a, b);
   }
-  for (let index = 0; index < size; index++) {
-    const parent = find(index);
-    const group = swapMap.get(parent);
+
+  for (let index = 0; index < n; index++) {
+    const group = uf.find(index);
+
+    if (!sourceMap.has(group)) {
+      sourceMap.set(group, new Map());
+    }
+
+    const valueMap = sourceMap.get(group);
     const value = source[index];
+    const count = valueMap.get(value) ?? 0;
 
-    group ? group.set(value, (group.get(value) ?? 0) + 1) : swapMap.set(parent, new Map([[value, 1]]));
+    valueMap.set(value, count + 1);
   }
-  for (let index = 0; index < size; index++) {
-    const parent = find(index);
-    const group = swapMap.get(parent);
+
+  for (let index = 0; index < n; index++) {
+    const group = uf.find(index);
+    const valueMap = sourceMap.get(group);
     const value = target[index];
-    const count = group.get(value);
+    const count = valueMap.get(value);
 
-    count ? group.set(value, count - 1) : (result += 1);
+    if (count) {
+      valueMap.set(value, count - 1);
+    } else {
+      result += 1;
+    }
   }
+
   return result;
 };
+
+class UnionFind {
+  constructor(n) {
+    this.groups = Array.from({ length: n }, (_, index) => index);
+    this.ranks = Array.from({ length: n }, () => 0);
+  }
+
+  find(x) {
+    if (this.groups[x] === x) return x;
+
+    this.groups[x] = this.find(this.groups[x]);
+
+    return this.groups[x];
+  }
+
+  union(x, y) {
+    const groupX = this.find(x);
+    const groupY = this.find(y);
+
+    if (groupX === groupY) return false;
+
+    if (this.ranks[groupX] > this.ranks[groupY]) {
+      this.groups[groupY] = groupX;
+    } else if (this.ranks[groupX] < this.ranks[groupY]) {
+      this.groups[groupX] = groupY;
+    } else {
+      this.groups[groupY] = groupX;
+      this.ranks[groupX] += 1;
+    }
+
+    return true;
+  }
+}
