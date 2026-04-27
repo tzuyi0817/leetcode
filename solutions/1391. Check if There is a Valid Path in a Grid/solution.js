8c77f121b126d756bbaaf1102ef3b9fe1a7f1350
tzuyi <a0988426059@gmail.com>
2026-04-27 09:42:17 +0800

    refactor(solutions): solution `1391. Check if There is a Valid Path in a Grid`

--- solutions/1391. Check if There is a Valid Path in a Grid/solution.js
+++ solutions/1391. Check if There is a Valid Path in a Grid/solution.js
@@ -5,38 +5,82 @@
 const hasValidPath = function (grid) {
   const m = grid.length;
   const n = grid[0].length;
-  const connectMap = { left: 'right', right: 'left', upper: 'lower', lower: 'upper' };
-  const moveMap = {
-    left: { row: 0, col: -1 },
-    right: { row: 0, col: 1 },
-    upper: { row: -1, col: 0 },
-    lower: { row: 1, col: 0 },
-  };
+  const start = grid[0][0];
+
+  if (start === 5) return false;
+
   const streetMap = {
-    1: ['left', 'right'],
-    2: ['upper', 'lower'],
-    3: ['left', 'lower'],
-    4: ['lower', 'right'],
-    5: ['left', 'upper'],
-    6: ['upper', 'right'],
+    1: (from, row, col) => {
+      if (from === 'up' || from === 'down') return false;
+
+      if (from === 'left') return { row, col: col + 1, to: 'left' };
+
+      return { row, col: col - 1, to: 'right' };
+    },
+    2: (from, row, col) => {
+      if (from === 'left' || from === 'right') return false;
+
+      if (from === 'up') return { row: row + 1, col, to: 'up' };
+
+      return { row: row - 1, col, to: 'down' };
+    },
+    3: (from, row, col) => {
+      if (from === 'up' || from === 'right') return false;
+
+      if (from === 'left') return { row: row + 1, col, to: 'up' };
+
+      return { row, col: col - 1, to: 'right' };
+    },
+    4: (from, row, col) => {
+      if (from === 'up' || from === 'left') return false;
+
+      if (from === 'right') return { row: row + 1, col, to: 'up' };
+
+      return { row, col: col + 1, to: 'left' };
+    },
+    5: (from, row, col) => {
+      if (from === 'down' || from === 'right') return false;
+
+      if (from === 'up') return { row, col: col - 1, to: 'right' };
+
+      return { row: row - 1, col, to: 'down' };
+    },
+    6: (from, row, col) => {
+      if (from === 'down' || from === 'left') return false;
+
+      if (from === 'up') return { row, col: col + 1, to: 'left' };
+
+      return { row: row - 1, col, to: 'down' };
+    },
   };
-  const isValidPath = (row, col, connect, visited = new Set()) => {
-    if (row >= m || col >= n || row < 0 || col < 0) return false;
-    if (visited.has(`${row}_${col}`)) return false;
-    const street = streetMap[grid[row][col]];
-    const startStreet = street.indexOf(connect);
-    const isConnect = startStreet !== -1;
-    if (!isConnect) return false;
+
+  const isValidPath = (row, col, from) => {
+    if (row < 0 || col < 0 || row >= m || col >= n) return false;
+
+    const current = grid[row][col];
+
+    if (current === 'x') return false;
+
+    const nextPos = streetMap[current](from, row, col);
+
+    if (!nextPos) return false;
+
     if (row === m - 1 && col === n - 1) return true;
 
-    const endStreet = startStreet ? street[0] : street[1];
-    const move = moveMap[endStreet];
-    const nextConnect = connectMap[endStreet];
+    grid[row][col] = 'x';
 
-    visited.add(`${row}_${col}`);
-    return isValidPath(row + move.row, col + move.col, nextConnect, visited);
+    const isValid = isValidPath(nextPos.row, nextPos.col, nextPos.to);
+
+    grid[row][col] = current;
+
+    return isValid;
   };
-  const [directionA, directionB] = streetMap[grid[0][0]];
 
-  return isValidPath(0, 0, directionA) || isValidPath(0, 0, directionB);
+  if (start === 4) {
+    return isValidPath(0, 0, 'right') || isValidPath(0, 0, 'down');
+  }
+
+  const fromMap = { 1: 'left', 2: 'up', 3: 'left', 6: 'up' };
+
+  return isValidPath(0, 0, fromMap[start]);
 };
