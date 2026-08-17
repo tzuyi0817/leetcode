f3d81b8ab2dd282f96e1c4cc9cac36268d580ecc
tzuyi <a0988426059@gmail.com>
2026-08-17 09:41:33 +0800

    refactor(solutions): solution `1563. Stone Game V`

--- solutions/1563. Stone Game V/solution.js
+++ solutions/1563. Stone Game V/solution.js
@@ -4,39 +4,44 @@
  */
 const stoneGameV = function (stoneValue) {
   const n = stoneValue.length;
-  const prefixScore = Array.from({ length: n + 1 }, () => 0);
+  const prefixSum = Array.from({ length: n + 1 }, () => 0);
   const dp = Array.from({ length: n }, () => new Array(n).fill(-1));
 
   for (let index = 1; index <= n; index++) {
-    prefixScore[index] = prefixScore[index - 1] + stoneValue[index - 1];
+    const value = stoneValue[index - 1];
+
+    prefixSum[index] = prefixSum[index - 1] + value;
   }
 
-  const getMaxScore = (left, right) => {
-    if (left >= right) return 0;
-    if (dp[left][right] !== -1) return dp[left][right];
+  const getMaxScore = (l, r) => {
+    if (l === r) return 0;
+
+    if (dp[l][r] !== -1) return dp[l][r];
+
     let result = 0;
 
-    for (let index = left; index < right; index++) {
-      const heap1 = prefixScore[index + 1] - prefixScore[left];
-      const heap2 = prefixScore[right + 1] - prefixScore[index + 1];
+    for (let index = l; index < r; index++) {
+      const mid = index + 1;
+      const leftValue = prefixSum[mid] - prefixSum[l];
+      const rightValue = prefixSum[r + 1] - prefixSum[mid];
 
-      if (heap1 > heap2) {
-        const score = heap2 + getMaxScore(index + 1, right);
+      if (leftValue > rightValue) {
+        const sum = rightValue + getMaxScore(mid, r);
 
-        result = Math.max(score, result);
-      } else if (heap1 < heap2) {
-        const score = heap1 + getMaxScore(left, index);
+        result = Math.max(sum, result);
+      } else if (leftValue < rightValue) {
+        const sum = leftValue + getMaxScore(l, index);
 
-        result = Math.max(score, result);
+        result = Math.max(sum, result);
       } else {
-        const score1 = heap2 + getMaxScore(index + 1, right);
-        const score2 = heap1 + getMaxScore(left, index);
+        const rightSum = rightValue + getMaxScore(mid, r);
+        const leftSum = leftValue + getMaxScore(l, index);
 
-        result = Math.max(result, score1, score2);
+        result = Math.max(leftSum, rightSum, result);
       }
     }
 
-    dp[left][right] = result;
+    dp[l][r] = result;
 
     return result;
   };
