da34fa408bda5cbe1718c0725d7e4304082dee35
tzuyi <a0988426059@gmail.com>
2026-09-16 11:11:55 +0800

    refactor(solutions): solution `1621. Number of Sets of K Non-Overlapping Line Segments`

--- solutions/1621. Number of Sets of K Non-Overlapping Line Segments/solution.js
+++ solutions/1621. Number of Sets of K Non-Overlapping Line Segments/solution.js
@@ -5,16 +5,23 @@
  */
 const numberOfSets = function (n, k) {
   const MODULO = 10 ** 9 + 7;
-  const combinatorics = (n, k) => {
-    const dp = new Array(n + 1).fill('').map(_ => new Array(k + 1).fill(1));
+  let noLine = Array.from({ length: n }, () => 1);
+  let inLine = Array.from({ length: n }, () => 0);
 
-    for (let index = 1; index <= n; index++) {
-      for (let point = 1; point < index && point <= k; point++) {
-        dp[index][point] = (dp[index - 1][point - 1] + dp[index - 1][point]) % MODULO;
-      }
+  for (let segment = 1; segment <= k; segment++) {
+    const nextNoLine = new Array(n).fill(0);
+    const nextInLine = new Array(n).fill(0);
+
+    for (let index = 1; index < n; index++) {
+      const prevTotal = (noLine[index - 1] + inLine[index - 1]) % MODULO;
+
+      nextInLine[index] = (nextInLine[index - 1] + prevTotal) % MODULO;
+      nextNoLine[index] = (nextNoLine[index - 1] + nextInLine[index - 1]) % MODULO;
     }
-    return dp[n][k];
-  };
 
-  return combinatorics(n + k - 1, k * 2);
+    noLine = nextNoLine;
+    inLine = nextInLine;
+  }
+
+  return (noLine[n - 1] + inLine[n - 1]) % MODULO;
 };
